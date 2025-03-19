818560a95376c26acf1bcf0fe32a0242a33dc72f
pima-pimpim <pimpisa.chrcht@gmail.com>
2025-03-19 09:46:14 +0700

    init

--- main.py
+++ main.py
@@ -2,25 +2,33 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide")
+
 st.title("Visualization Heatmap")
 
-#uploaded_file = st.file_uploader("Choose a file")
-#if uploaded_file is not None:
-#    # To read file as bytes:
-#    bytes_data = uploaded_file.getvalue()
-#    st.write(bytes_data)
-#
-#    # To convert to a string based IO:
-#    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-#    st.write(stringio)
-#
-#    # To read file as string:
-#    string_data = stringio.read()
-#    st.write(string_data)
-#
-#    # Can be used wherever a "file-like" object is accepted:
-#    dataframe = pd.read_csv(uploaded_file)
-#    st.write(dataframe)
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    #bytes_data = uploaded_file.getvalue()
+    #st.write(bytes_data)
+
+    # To convert to a string based IO:
+    #stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    #st.write(stringio)
+
+    # To read file as string:
+    #string_data = stringio.read()
+    #st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    try:
+        dataframe = pd.read_csv(uploaded_file)
+    except:
+        dataframe = pd.read_excel(uploaded_file)
+    st.markdown(dataframe.head())
 
 
 #option = st.selectbox(
